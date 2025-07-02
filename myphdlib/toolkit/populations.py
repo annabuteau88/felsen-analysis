bb99777997f82bde96bcc2e99e2ac80b6a73d120
jbhunt <hunt.brian.joshua@gmail.com>
2025-07-02 11:38:03 -0600

    finalized premotor & visual functions & made changes to process.py to make everything work

--- myphdlib/toolkit/populations.py
+++ myphdlib/toolkit/populations.py
@@ -1,26 +1,28 @@
-population = Population()
-#for unit in population:
-    #spikeTimes = unit.timestamps
-    #spikeCluster = unit.cluster
+from toolkit.process import AnalysisObject
+from interface.ephys import Population
+import unit_localizer as ul
+import numpy as np
 
-def definePremotorPopulation(self, h5file):
+def definePremotorPopulation(h5file, clusterFile):
     """
     This function filters your population of neurons and pulls out premotor neurons based on ZETA test results
     """
-    session = AnalysisObject()
+    session = AnalysisObject(h5file)
     population = Population(session)
-    labels = session.load(h5file, 'nptracer/labels')
-    transformed = session.load(h5file, 'nptracer/transformed')
+    labels = session.load('nptracer/labels')
+    transformed = session.load('nptracer/transformed')
     brainAreas = ul.translateBrainAreaIdentities(labels) 
-    spikeClustersFile = session.home.joinpath('ephys/sorting/manual/spike_clusters.npy') #fix
+    #spikeClustersFile = session.home.joinpath('ephys/sorting/manual/spike_clusters.npy') #fix
+    spikeClustersFile = clusterFile
     uniqueSpikeClusters = np.unique(np.load(spikeClustersFile))
-    zetaNasal = session.load(h5file, 'zeta/saccade/nasal/p')
-    zetaTemporal = session.load(h5file, 'zeta/saccade/temporal/p')
-    ampCutoff = session.load(h5file, 'metrics/ac')
-    presenceRatio = session.load(h5file, 'metrics/pr')
-    firingRate = session.load(h5file, 'metrics/fr')
-    isiViol = session.load(h5file, 'metrics/rpvr')
-    qualityLabels = session.load('h5file, metrics/ql')
+    zetaNasal = session.load('zeta/saccade/nasal/p')
+    zetaTemporal = session.load('zeta/saccade/temporal/p')
+    ampCutoff = session.load('metrics/ac')
+    presenceRatio = session.load('metrics/pr')
+    firingRate = session.load('metrics/fr')
+    isiViol = session.load('metrics/rpvr')
+    qualityLabels = session.load('metrics/ql')
+    premotorUnitsZeta = list()
     for index, pVal in enumerate(zetaNasal):
         if brainAreas[index] in ['SCsg','SCop', 'SCig', 'SCiw', 'SCdg']:
             pNasal = pVal
@@ -41,3 +43,41 @@ def definePremotorPopulation(self, h5file):
 
     return premotorUnitsZeta
     
+def defineVisualPopulation(h5file, clusterFile):
+    """
+    This function filters your population of neurons and pulls out visual neurons based on ZETA test results
+    """
+    session = AnalysisObject(h5file)
+    population = Population(session)
+    labels = session.load('nptracer/labels')
+    transformed = session.load('nptracer/transformed')
+    #spikeClustersFile = session.home.joinpath('ephys/sorting/manual/spike_clusters.npy')
+    spikeClustersFile = clusterFile
+    uniqueSpikeClusters = np.unique(np.load(spikeClustersFile))
+    brainAreas = ul.translateBrainAreaIdentities(labels) 
+    zetaLeft = session.load('zeta/probe/left/p')
+    zetaRight = session.load('zeta/probe/right/p')
+    ampCutoff = session.load('metrics/ac')
+    presenceRatio = session.load('metrics/pr')
+    firingRate = session.load('metrics/fr')
+    isiViol = session.load('metrics/rpvr')
+    qualityLabels = session.load('metrics/ql')
+    visualUnitsZeta = list()
+    for index, pVal in enumerate(zetaLeft):
+        if brainAreas[index] in ['SCsg','SCop', 'SCig', 'SCiw', 'SCdg']:
+            pLeft = pVal
+            pRight = zetaRight[index]
+            if pLeft < pRight:
+                p = pLeft
+            elif pRight < pLeft:
+                p = pRight
+            if p < 0.05:
+                if qualityLabels is not None and qualityLabels[index] in (0, 1):
+                        continue
+                if ampCutoff[index] <= 0.1:
+                    if presenceRatio[index] >= 0.9:
+                        if firingRate[index] >= 0.2:
+                            if isiViol[index] <= 0.5:
+                                unit = uniqueSpikeClusters[index]
+                                visualUnitsZeta.append(unit)
+    return visualUnitsZeta
--- myphdlib/toolkit/process.py
+++ myphdlib/toolkit/process.py
@@ -1,23 +1,38 @@
+import h5py
+import numpy as np
+
 class AnalysisObject():
     """
     This class is used in the background of the toolkit modules. 
     It has helper properties to make the code more readable & writeable.
     """
-    def load(self, h5file, path):
+    def __init__(self, h5file):
+        self.hdf = h5file
+        return
+    
+    def load(self, path):
         """
         This is a helper function for easily loading h5 files
         """
         obj = None
-        with h5py.File(str(h5file), 'r') as file:
+        with h5py.File(str(self.hdf), 'r') as file:
             try:
                 obj = file[path]
                 if type(obj) == h5py.Dataset:
-                    if returnMetadata:
-                        return np.array(obj), dict(obj.attrs)
-                    else:
-                        return np.array(obj)
+                   return np.array(obj)
                 elif type(obj) == h5py.Group:
                     return obj
             except KeyError:
                 pass
-        
+    
+    def hasDataset(self, path):
+        """
+        Looks for a group/dataset in the output file
+        """
+
+        with h5py.File(self.hdf, 'r') as file:
+            try:
+                dataset = file[path]
+                return True
+            except KeyError:
+                return False
